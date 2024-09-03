c362326f5e1e73cd20171b67d2312a95e5b7d3b8
MungChi <2ee42a45@gmail.com>
2024-09-04 08:58:02 +0900

    Feat: login 파일

---
+++ login/src/test/java/com/backend/voicetuner/VoiceTunerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.voicetuner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceTunerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
